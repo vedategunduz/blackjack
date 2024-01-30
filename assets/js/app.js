f0d97284f02793ea35dbacdcd5ec27b30c422674
vedategunduz <vedat.bilisim@outlook.com>
2024-01-31 01:21:23 +0300

    Changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,55 @@
+// Kart değerleri ve türleri tanımlanıyor
 const card = {
-    "values": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-    "types": ["Clubs", "Diamonds", "Hearts", "Spades"]
-}
+    values: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+    types: ["Clubs", "Diamonds", "Hearts", "Spades"]
+};
 
+// Çekilebilecek kartlar ve seçilen kartlar için boş diziler oluşturuluyor
 let cards = [];
 let selectedCards = [];
 
+// Oyuncuların kart bilgilerini tutacak nesne oluşturuluyor
+const playerCardInfos = {};
+
+// Oyuncu isimleri tanımlanıyor
+const playerNames = ["You", "Courpier"];
+
+// Her oyuncu için boş bir kart listesi oluşturuluyor
+playerNames.forEach(name => {
+    playerCardInfos[name] = [];
+});
+
+// Oyuncuya kart çekme işlemi
+function hit(player) {
+    // Kart çekiliyor
+    const randomIndex = random(0, cards.length);
+    const cardDrawn = cards[randomIndex];
+
+    // Eğer çekilen kart daha önce çekilmediyse
+    if (!selectedCards.includes(cardDrawn) && cards.length > 0) {
+        // Çekilen kartı listeden çıkar
+        cards.splice(randomIndex, 1);
+        // Çekilen kartı kaydet
+        selectedCards.push(cardDrawn);
+        playerCardInfos[player].push(cardDrawn);
+    } else if (cards.length > 0) {
+        // Eğer çekilen kart daha önce çekildiyse, tekrar kart çek
+        hit(player);
+    } else {
+        console.error("Yeni deste gerekiyor.");
+    }
+    // Oyuncu kart bilgilerini konsola yazdır
+    console.log(playerCardInfos);
+}
+
+// Rastgele sayı üreteci fonksiyonu
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Kart deste oluşturma
 card.types.forEach(type => {
     card.values.forEach(val => {
         cards.push(`${type} ${val}`);
     });
 });
-
-let playerCardInfos = new Object();
-let you = true;
-const playerNames = ["You", "Courpier"];
-
-playerNames.forEach(name => {
-    playerCardInfos[name] = [];
-});
-
-function hit() {
-    //  Kart çek
-    const card = cards[random(0, cards.length)];
-    const index = cards.indexOf(card);
-
-    if (!selectedCards.includes(card) && cards.length > 0) {
-        // Eğer çekilen kart daha önceden çekilmemiş ise
-        // Çekilen kartı listeden çıkart
-        cards.splice(index, 1);
-        // Çekilen kartı kaydet
-        selectedCards.push(card);
-        if (you) {
-            playerCardInfos.You.push(card);
-        } else {
-            playerCardInfos.Courpier.push(card);
-        }
-    }
-    else if (cards.length > 0) {
-        hit();
-    }
-    else {
-        console.error("Yeni deste oluşturulması lazım.");
-    }
-    console.log(playerCardInfos);
-}
-console.log(cards);
-// Random number generator
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;;
-}
